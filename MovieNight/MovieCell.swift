534ad3ff45e537cb09d7eb6429512008266543e4
jkim502 <Jeesoo@Jeesoos-MacBook-Pro.local>
2017-02-06 22:13:16 -0500

    update

---
+++ MovieNight/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieNight
+//
+//  Created by Jeesoo Kim on 2/5/17.
+//  Copyright © 2017 Jeesoo Kim. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
